93c3a4a92591cf5a7e9155cdd2881684859d84f7
valeriaherrera <valeherrera133@gmail.com>
2024-10-14 17:07:34 -0500

    course tile component created

--- course-planner/src/main.jsx
+++ course-planner/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './components/Header/Header.jsx'
 import './styles.css'
+import CourseList from './components/Course-Tile/CourseList.jsx'
+import CourseTile from './components/Course-Tile/CourseTile.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header/>
+    <CourseList/>
   </StrictMode>,
 )
