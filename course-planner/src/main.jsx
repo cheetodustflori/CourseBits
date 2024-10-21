92979c36484bc2676d890b2355d02f26ed0f3f76
Michelle Ye <mye21@uic.edu>
2024-10-21 14:23:25 -0500

    Updates Header Code to adjust to correct folder access

--- course-planner/src/main.jsx
+++ course-planner/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Header from './Header.jsx'
+import Header from './components/Header/Header.jsx';
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
