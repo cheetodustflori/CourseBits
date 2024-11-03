49c384c1bde615c5e07cbdd2ec0e2d116902b12f
valeriaherrera <valeherrera133@gmail.com>
2024-11-03 17:18:52 -0600

    merged header code and updated json file to branch

--- course-planner/src/main.jsx
+++ course-planner/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Header from './components/Header/Header.jsx'
-import './styles.css'
+import './components/Header/styles.css'
 import CourseList from './components/Course-Tile/CourseList.jsx'
 import CourseTile from './components/Course-Tile/CourseTile.jsx'
 
