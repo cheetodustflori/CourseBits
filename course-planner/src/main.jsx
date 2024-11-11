fbd12691ed9d4b7772f492fd30529243d83b15f3
cheetodustflori <florianneche@gmail.com>
2024-11-11 12:19:19 -0600

    updating main

--- course-planner/src/main.jsx
+++ course-planner/src/main.jsx
@@ -9,7 +9,6 @@ import { createRoot } from 'react-dom/client'
 import Header from './components/Header/Header.jsx'
 import './components/Header/styles.css'
 import CourseList from './components/Course-Tile/CourseList.jsx'
-import CourseTile from './components/Course-Tile/CourseTile.jsx'
 
 
 // NOTE CHANGE PATH FOR DETAILS PAGE LATER TO /details and 
@@ -17,19 +16,6 @@ import CourseTile from './components/Course-Tile/CourseTile.jsx'
 // RN this just easiest way to see code
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-// <<<<<<< course-details
-    
-//     <Router>
-//       <Routes>
-//         {/* Route for Header page */}
-//         <Route path="/temp" element={<Header />} />
-
-//         {/* Route for Details page */}
-//         <Route path="/" element={<Details />} />
-//       </Routes>
-//     </Router>
-//   </StrictMode>
-// );
     <Header/>
     <CourseList/>
   </StrictMode>,
