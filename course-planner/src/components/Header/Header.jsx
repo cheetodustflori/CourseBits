a79e970a3b9ba5c04f057502e691180f2d73ef92
cheetodustflori <florianneche@gmail.com>
2024-11-14 15:50:41 -0600

    updating css

--- course-planner/src/components/Header/Header.jsx
+++ course-planner/src/components/Header/Header.jsx
@@ -2,33 +2,37 @@ import "./styles.css";
 
 export default function Header() {
   return (
-        <body>
-        <header>
-          <div id="red-box"></div>
+    <body>
+      <header>
+        <div id="red-box"></div>
 
-          <div id="circle" className="circle">
-            <h3 id="uic-text">UIC</h3>
-          </div>
-
-          <div id="text">Computer Science Courses: By the Students</div>
-        </header>
-        
-        <div class = "button-container" id = "buttonspot">
-          <button class = "button"> Engineering Requirements</button>
-          <button class = "button"> Technical Electives</button>
+        <div id="circle" className="circle">
+          <h3 id="uic-text">UIC</h3>
         </div>
 
-        <div class = "sidebar">
-          <h3>Difficulty Rating</h3>
-        <div class = "rating">
-          <span class = "difficulty-bar"></span>
-        </div>
-          <h3>Student Favorites</h3>
-          <span class = "favorite-icon"><img src="/../assets/star.jpg" style={{width: 25, height: 25}}></img></span>
-          <h3>Course Topics</h3>
-          <span class = "course-topic">Topic</span>
-        </div> 
+        <div id="text">Computer Science Courses: By the Students</div>
+      </header>
 
-        </body>
+      <div class="button-container" id="buttonspot">
+        <button class="button"> Engineering Requirements</button>
+        <button class="button"> Technical Electives</button>
+      </div>
+
+      <div class="sidebar">
+        <h3>Difficulty Rating</h3>
+        <div class="rating">
+          <span class="difficulty-bar"></span>
+        </div>
+        <h3>Student Favorites</h3>
+        <div class="favorite-icon">
+          <img
+            src="/../assets/star.jpg"
+            style={{ width: 25, height: 25 }}
+          ></img>
+        </div>
+        <h3>Course Topics</h3>
+        <span class="course-topic">Topic</span>
+      </div>
+    </body>
   );
 }
