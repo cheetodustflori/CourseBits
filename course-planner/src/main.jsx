ec49002c0a648a8e526dfc391bef0428eba4ff6d
cheetodustflori <florianneche@gmail.com>
2024-11-14 14:17:25 -0600

    yee

--- course-planner/src/main.jsx
+++ course-planner/src/main.jsx
@@ -3,16 +3,25 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CourseList from './components/Course-Tile/CourseList';
 import Details from './components/Course-Details/Details';
+import Header from './components/Header/Header';
 
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<CourseList />} />
-                <Route path="/course/:id" element={<Details />} />
-            </Routes>
-        </BrowserRouter>
-    );
+function HomePage() {
+  return (
+      <>
+          <Header />
+          <CourseList />
+      </>
+  );
 }
 
+function App() {
+  return (
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/course/:id" element={<Details />} />
+          </Routes>
+      </BrowserRouter>
+  );
+}
 ReactDOM.render(<App />, document.getElementById('root'));
