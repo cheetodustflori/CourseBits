dc9e1a91fa4421360911da4cacbf012c243e5a0b
Midnight Web <csanvaz77@gmail.com>
2024-11-15 14:04:35 -0600

    Finalized course-description component

--- course-planner/src/main.jsx
+++ course-planner/src/main.jsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CourseList from './components/Course-Tile/CourseList';
 import Details from './components/Course-Details/Details';
 import Header from './components/Header/Header';
+import CourseCard from './components/Course-Description/course-description';
 
 function HomePage() {
   return (
-      <>
-          <Header />
-          <CourseList />
-      </>
+    <>
+       <Header />
+      <CourseList />
+    </>
   );
 }
 
 function App() {
   return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/course/:id" element={<Details />} />
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/course/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// Create a root
+const root = createRoot(document.getElementById('root'));
+
+// Render the App
+root.render(<React.StrictMode><App /></React.StrictMode>);
