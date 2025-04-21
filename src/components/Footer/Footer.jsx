5536a374e00b3d2fda5af59a115890e9d11d0a1d
cheetodustflori <florianneche@gmail.com>
2025-04-21 10:54:33 -0500

    minor changes

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.css';
+
+export default function Footer() {
+    return (
+        <div className={styles.footer}>
+            <p>Made with&nbsp;
+                <a style={{color: '#FF8181'}} href="https://github.com/cheetodustflori/UIC-Course-Planner">{`<`}3</a>
+                &nbsp;by the WiCS Dev Team © Fall 2024</p> 
+        </div>
+    )
+};
